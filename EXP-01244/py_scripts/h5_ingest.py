03f5a9921d79a18e38733e10766b67238a29421e
root <root@ide-0.ide.default.svc.cluster.local>
2025-01-22 22:28:36 +0000

    1/22/25 backup

--- EXP-01244/py_scripts/h5_ingest.py
+++ EXP-01244/py_scripts/h5_ingest.py
@@ -11,4 +11,14 @@ EXP01244_cr_outs_path = os.path.join(hdir, "TissDiss/EXP-01244/EXP-01244_cr_outs
 
 adata = ingest_mplex_gex_h5_list(EXP01244_cr_outs_path)
 
+sample_dict = {
+    'week2': 'Week 2',
+    'week3': 'Week 3',
+    'week4': 'Week 4',
+    'bm': 'BMMC Start Sample',
+    'msc': 'MSC Start Sample'
+}
+
+adata.obs['names'] = adata.obs['sample_type'].replace(sample_dict)
+
 adata.write(srldir + 'raw_adata.h5ad', compression='gzip')
