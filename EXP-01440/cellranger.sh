8d75e4ec3a88ca215cf2fb2fa5bbe054e2a60f05
root <root@multiplemyeloma-0.multiplemyeloma.default.svc.cluster.local>
2025-03-12 11:43:27 -0700

    3/12/25 backup

--- EXP-01440/cellranger.sh
+++ EXP-01440/cellranger.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
 home="/home/workspace"
-config="${home}/mm_analysis/EXP-01440/EXP-01440_config.csv"
+config="${home}/mm_bm_chip_organoids/EXP-01440/EXP-01440_config.csv"
 cr="${home}/cellranger-8.0.1/cellranger"
-cr_outs="${home}/mm_analysis/EXP-01440/cr_outs"
+cr_outs="${home}/mm_bm_chip_organoids/EXP-01440/cr_outs"
+
+mkdir -p ${cr_outs}
 
 ${cr} multi --id='EXP-01440' \
     --csv=${config} \
     --output-dir=${cr_outs} \
     --localcores=60
+
+tar -czvf ~/private/mm_analysis/EXP-01440_cr_outs.tar.gz ${cr_outs}
